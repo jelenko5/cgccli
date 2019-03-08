5a76b64a1c793b7c61a75812aeee67ad0d519e46
jelenko <jelenko555@gmail.com>
2019-03-08 20:37:45 +0100

    add `config` subcommand

--- utils/logger.py
+++ utils/logger.py
@@ -4,6 +4,10 @@ debug_logging = False
 debug_style = dict()
 
 
+def is_debug_mode():
+    return debug_logging
+
+
 def debug(output):
     if debug_logging:
         click.echo(click.style(output, **debug_style))
